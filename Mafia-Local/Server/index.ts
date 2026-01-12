23c207e0a8903a26a86634455090655a9855f132
castrox2 <151793182+castrox2@users.noreply.github.com>
2026-01-12 01:36:55 -0700

    Server?
    Started something when it comes to servers and stuff (its literally just the start of what its meant to be)

--- Mafia-Local/Server/index.ts
+++ Mafia-Local/Server/index.ts
@@ -1,4 +1,8 @@
-const io = require('socket.io')(3000)
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: "5500",
+    }
+})
 
 io.on("connection", (socket: { id: any }) => {
     console.log("New client connected:", socket.id)
